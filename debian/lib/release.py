07d010c50416169ebdbae07d0d65cf3cfcd9bc62
Philippe Thierry <phil@reseau-libre.net>
2017-02-28 21:51:09 +0100

    add support for upstream changelog

--- debian/lib/release.py
+++ debian/lib/release.py
@@ -7,7 +7,7 @@ import json
 r = requests.get('https://api.github.com/repos/OpenSCAP/scap-security-guide/releases/tags/v0.1.31')
 if(r.ok):
     repoItem = json.loads(r.text or r.content)
-    with codecs.open("debian/ReleaseNotes.md", "w", "utf-8") as file:
+    with codecs.open("changelog", "w", "utf-8") as file:
         file.write("# scap-security-guide" + " " + repoItem['tag_name'] + "\r\n")
         file.write("\r\n")
         file.write(repoItem['body'])

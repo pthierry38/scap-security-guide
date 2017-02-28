9ce83210981daf6f6658919bd32554158b75347d
Philippe Thierry <phil@reseau-libre.net>
2017-02-28 20:10:01 +0100

    updated upstream release note generation from github

--- debian/lib/release.py
+++ debian/lib/release.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import requests
+import codecs
 import json
 
 r = requests.get('https://api.github.com/repos/OpenSCAP/scap-security-guide/releases/tags/v0.1.31')
 if(r.ok):
     repoItem = json.loads(r.text or r.content)
-    print "# scap-security-guide" + " " + repoItem['tag_name']
-    print ""
-    print repoItem['body']
+    with codecs.open("debian/ReleaseNotes.md", "w", "utf-8") as file:
+        file.write("# scap-security-guide" + " " + repoItem['tag_name'] + "\r\n")
+        file.write("\r\n")
+        file.write(repoItem['body'])
